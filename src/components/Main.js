4830cf6d2e66d7bc17c1a9b20e34b82248e5c7e6
jennypham <64534681+jennyberry@users.noreply.github.com>
2022-10-23 21:05:57 +1100

    select dice and update roll btn func

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,33 +2,44 @@ import React from "react";
 import Dice from "./Dice";
 import { nanoid } from "nanoid"; // dependency to generate unique id
 export default function Main(props) {
+  const [diceArr, setDiceArr] = React.useState(allnewDice());
+  function generateNewDice() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
   function allnewDice() {
+    //normal JS func to generate an array of 10 random numbers
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDice());
+    }
+    return newDice;
     // //ES6 fast way to generate an array of 10 random numbers
     // return Array(10)
     //   .fill()
     //   .map(() => Math.floor(Math.random() * 6) + 1);
-
-    //normal JS func to generate an array of 10 random numbers
-    const newDice = [];
-    for (let i = 0; i < 10; i++) {
-      newDice.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid(),
-      });
-    }
-    return newDice;
   }
 
   //func to get new array when click Roll button
   function rollDice() {
-    setDiceArr(allnewDice());
+    setDiceArr((oldDie) =>
+      oldDie.map((dice) => {
+        return dice.isHeld ? dice : generateNewDice();
+      })
+    );
   }
   //func to get dice's id when click each dice
   function holdDice(id) {
-    console.log(id);
+    setDiceArr((oldDie) =>
+      oldDie.map((dice) => {
+        return dice.id === id ? { ...dice, isHeld: !dice.isHeld } : dice;
+      })
+    );
   }
-  const [diceArr, setDiceArr] = React.useState(allnewDice());
+
   //Map array to die components
   const diceElement = diceArr.map((dice) => (
     <Dice
